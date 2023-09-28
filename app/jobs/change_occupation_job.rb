659d4f1595dccf9be6f47a6e9894ca31aa864666
Seenivasan <srinirio828@gmail.com>
2023-09-28 17:02:40 +0530

    changing the database sqlite to postgres

--- app/jobs/change_occupation_job.rb
+++ app/jobs/change_occupation_job.rb
@@ -2,11 +2,10 @@ class ChangeOccupationJob < ApplicationJob
   queue_as :default
 
   def perform(parking_slot)
-
-
-    parking_slot.update(occupied: false)
-
-    p "hello- #{parking_slot.id}"
+    if parking_slot.update(occupied: false)
+      p "Successfully updated parking_slot #{parking_slot.id}"
+    else
+      p "Failed to update parking_slot #{parking_slot.id}: #{parking_slot.errors.full_messages.join(', ')}"
+    end
   end
-
 end
